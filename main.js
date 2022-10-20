7d633af113b0e50e4be6bd606275cd3a336f539e
Gleison <gleison04lemos@gmail.com>
2022-10-20 14:29:00 -0300

    Funções

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-document.querySelector('.tecla_pom')
+function playPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = playPom;
+
+function playClap () {
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla_clap').onclick = playClap;
